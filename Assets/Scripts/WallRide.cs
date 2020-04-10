c9a9bdce44c9338c3664c7662d596978bffa68ae
Yoshi <yoshua.woo@gmail.com>
2020-04-10 12:24:34 +0200

    Started, then paused development on a wall run;

---
+++ Assets/Scripts/WallRide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRide : MonoBehaviour
+{
+    public float WallCheckDistance;
+    public LayerMask WallLayerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //DetectWalls();
+    }
+
+    // RaycastHit DetectWalls()
+    // {
+    //     RaycastHit hitRight;
+    //     RaycastHit hitLeft;
+    //     if (Physics.Raycast(transform.position, transform.right, out hitRight, WallCheckDistance, WallLayerMask))
+    //     {
+    //         return hitRight;
+    //     }
+    //     Physics.Raycast(transform.position, -transform.right, out hitLeft, WallCheckDistance, WallLayerMask);
+    //     Debug.DrawRay(transform.position, transform.right * WallCheckDistance, Color.red);
+    //     Debug.DrawRay(transform.position, -transform.right * WallCheckDistance, Color.red);
+    // }
+}
