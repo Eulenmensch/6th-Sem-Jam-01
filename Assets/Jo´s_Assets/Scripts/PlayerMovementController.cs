57ff20b98ca8ae75979f0fdd829124a673bd9072
Jo <papejo@hotmail.de>
2020-04-14 15:23:26 +0200

    Duplicated Yoshis Scene to have my own: "Scene_Jo" Added Animator and BlendTree Added Folder Jo´s_Assets Added Character Meshes and Animations

---
+++ Assets/Jo´s_Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private Animator _animator;
+
+    public void GetMoveInput(InputAction.CallbackContext context)
+        {
+            var inputVector = context.ReadValue<Vector2>();
+            _animator.SetFloat("velXFloat", inputVector.x);
+            _animator.SetFloat("velYFloat", inputVector.y);
+        }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+}
